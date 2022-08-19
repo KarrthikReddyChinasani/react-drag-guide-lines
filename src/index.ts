5f43881f7c9ad16c51e05b943466b0d1612dcd4d
Karthik Reddy <karthikreddy@zuddl.com>
2022-08-19 07:09:14 +0530

    add github pages

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import ReactAlignLinesContainer from './components/react-align-lines-container'
-import { IDragOperations } from './common/types'
-
-export { ReactAlignLinesContainer, IDragOperations }
